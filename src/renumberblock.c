dcf47fc1db87160ea0237378e2ba183d277118cb
Bert Timmerman <bert.timmerman@xs4all.nl>
2008-05-25 09:18:18 +0200

    Added renumberblock.c

---
+++ src/renumberblock.c
@@ -0,0 +1,93 @@
+/* RenumberBlock plug-in for PCB
+   http://www.delorie.com/pcb/renumberblock.c
+
+   Copyright (C) 2006 DJ Delorie <dj@delorie.com>
+
+   Licensed under the terms of the GNU General Public License, version
+   2 or later.
+
+   Compile like this:
+
+   gcc -I$HOME/geda/pcb-cvs/src -I$HOME/geda/pcb-cvs -O2 -shared renumberblock.c -o renumberblock.so
+
+   The resulting renumberblock.so goes in $HOME/.pcb/plugins/renumberblock.so.
+
+   Usage: RenumberBlock(oldnum,newnum)
+
+   All selected elements are renumbered by adding (newnum-oldnum) to
+   the existing number.  I.e. RenumberBlock(100,200) will change R213
+   to R313.
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+#include "global.h"
+#include "data.h"
+#include "hid.h"
+#include "misc.h"
+#include "create.h"
+#include "rtree.h"
+#include "undo.h"
+
+static int
+renumber_block (int argc, char **argv, int x, int y)
+{
+  char num_buf[15];
+  int old_base, new_base;
+
+  if (argc < 2) {
+    Message("Usage: RenumberBlock oldnum newnum");
+    return 1;
+  }
+
+  old_base = atoi (argv[0]);
+  new_base = atoi (argv[1]);
+
+  SET_FLAG (NAMEONPCBFLAG, PCB);
+
+  ELEMENT_LOOP (PCB->Data);
+  {
+    char *refdes_split, *cp;
+    char *old_ref, *new_ref;
+    int num;
+
+    if (!TEST_FLAG (SELECTEDFLAG, element))
+      continue;
+
+    old_ref = element->Name[1].TextString;
+    for (refdes_split=cp=old_ref; *cp; cp++)
+      if (!isdigit(*cp))
+	refdes_split = cp+1;
+
+    num = atoi (refdes_split);
+    num += (new_base - old_base);
+    sprintf(num_buf, "%d" ,num);
+    new_ref = (char *) malloc (refdes_split - old_ref + strlen(num_buf) + 1);
+    memcpy (new_ref, old_ref, refdes_split - old_ref);
+    strcpy (new_ref + (refdes_split - old_ref), num_buf);
+
+    AddObjectToChangeNameUndoList (ELEMENT_TYPE, NULL, NULL,
+				   element,
+				   NAMEONPCB_NAME (element));
+    
+    ChangeObjectName (ELEMENT_TYPE, element, NULL, NULL, new_ref);
+  }
+  END_LOOP;
+  IncrementUndoSerialNumber ();
+  return 0;
+}
+
+static HID_Action renumber_block_action_list[] = {
+  {"RenumberBlock", NULL, renumber_block,
+   NULL, NULL}
+};
+
+REGISTER_ACTIONS (renumber_block_action_list)
+
+void
+pcb_plugin_init()
+{
+  register_renumber_block_action_list();
+}
