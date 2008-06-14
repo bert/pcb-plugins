de65aafaea2dc561a01b74f90d5e8903ebe72f8a
Bert Timmerman <bert.timmerman@xs4all.nl>
2008-06-14 10:23:29 +0200

    Added src/lockelements.c

---
+++ src/lockelements.c
@@ -0,0 +1,145 @@
+/*!
+ * \file lockelements.c
+ * \author Copyright (C) 2008 by Bert Timmerman <bert.timmerman@xs4all.nl>
+ * \brief LockElements plug-in for PCB.
+ *
+ * Function to lock/unlock all/selected PCB elements.
+ * \n
+ * Compile like this:\n
+ * \n
+ * gcc -Ipath/to/pcb/src -Ipath/to/pcb -O2 -shared lockelements.c -o lockelements.so\n
+ * \n
+ * The resulting lockelements.so file should go in $HOME/.pcb/plugins/\n
+ *
+ * \warning Be very strict in compiling this plug-in against the exact pcb
+ * sources you compiled/installed the pcb executable (i.e. src/pcb) with.\n
+ * \n
+ * Usage: LockElements([Selected|All])\n
+ * Usage: UnlockElements([Selected|All])\n
+ * \n
+ * If no argument is passed, no locking/unlocking of elements is carried out.\n
+ * <hr>
+ * This program is free software; you can redistribute it and/or modify\n
+ * it under the terms of the GNU General Public License as published by\n
+ * the Free Software Foundation; either version 2 of the License, or\n
+ * (at your option) any later version.\n
+ * \n
+ * This program is distributed in the hope that it will be useful,\n
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of\n
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.\n
+ * \n
+ * You should have received a copy of the GNU General Public License\n
+ * along with this program; if not, write to:\n
+ * the Free Software Foundation, Inc.,\n
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.\n
+ */
+
+
+#include <stdio.h>
+#include <math.h>
+
+#include "global.h"
+#include "data.h"
+#include "hid.h"
+#include "misc.h"
+#include "create.h"
+#include "rtree.h"
+#include "undo.h"
+#include "set.h"
+
+/*!
+ * \brief Locking all or selected elements.
+ *
+ * Usage: LockElements([Selected|All])\n
+ *
+ * If no argument is passed, no action is carried out.
+ */
+static int
+lock_elements(int argc, char **argv)
+{
+        int selected;
+        int all;
+        if (argc > 0 && strcasecmp (argv[0], "Selected") == 0)
+                selected = 1;
+        else if (argc >0 && strcasecmp (argv[0], "All") == 0)
+                all = 1;
+        else
+        {
+                Message ("ERROR: in LockElements argument should be either Selected or All.\n");
+                return 1;
+        }
+        ELEMENT_LOOP(PCB->Data);
+        {
+                if (!TEST_FLAG (LOCKFLAG, element))
+                {
+                        /* element is not locked */
+                        if (all)
+                                SET_FLAG(LOCKFLAG, element);
+                        if (selected)
+                        {
+                                if (TEST_FLAG (SELECTEDFLAG, element))
+                                        SET_FLAG(LOCKFLAG, element);
+                        }
+                }
+        }
+        END_LOOP;
+}
+
+
+/*!
+ * \brief Locking all or selected elements.
+ *
+ * Usage: UnlockElements([Selected|All])\n
+ *
+ * If no argument is passed, no action is carried out.
+ */
+static int
+unlock_elements(int argc, char **argv)
+{
+        int selected;
+        int all;
+        if (argc > 0 && strcasecmp (argv[0], "Selected") == 0)
+                selected = 1;
+        else if (strcasecmp (argv[0], "All") == 0)
+                all = 1;
+        else
+        {
+                Message ("ERROR: in UnlockElements argument should be either Selected or All.\n");
+                return 1;
+        }
+        ELEMENT_LOOP(PCB->Data);
+        {
+                if (TEST_FLAG (LOCKFLAG, element))
+                {
+                        /* element is locked */
+                        if (all)
+                                CLEAR_FLAG(LOCKFLAG, element);
+                        if (selected)
+                        {
+                                if (TEST_FLAG (SELECTEDFLAG, element))
+                                        CLEAR_FLAG(LOCKFLAG, element);
+                        }
+                }
+        }
+        END_LOOP;
+}
+
+
+static HID_Action lockelements_action_list[] =
+{
+        {"LockElements", NULL, lock_elements, "Lock selected or all elements", NULL},
+        {"UnlockElements", NULL, unlock_elements, "Unlock selected or all elements", NULL}
+};
+
+
+REGISTER_ACTIONS (lockelements_action_list)
+
+
+void
+pcb_plugin_init()
+{
+        register_lockelements_action_list();
+}
+
+/* EOF */
