d7768b7bdbfe4523d182310b9dba01c12ff900ba
bert <bert.timmerman@xs4all.nl>
2014-03-20 08:22:18 +0100

    renumberblock: Doxygen tags added and changes in whitespace.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/plugins/renumberblock.c
+++ src/plugins/renumberblock.c
@@ -1,28 +1,32 @@
-/* RenumberBlock plug-in for PCB
-   http://www.delorie.com/pcb/renumberblock.c
-
-   Copyright (C) 2006 DJ Delorie <dj@delorie.com>
-
-   Licensed under the terms of the GNU General Public License, version
-   2 or later.
-
-   Compile like this:
-
-   gcc -I$HOME/geda/pcb-cvs/src -I$HOME/geda/pcb-cvs -O2 -shared renumberblock.c -o renumberblock.so
-
-   The resulting renumberblock.so goes in $HOME/.pcb/plugins/renumberblock.so.
-
-   Usage: RenumberBlock(oldnum,newnum)
-
-   All selected elements are renumbered by adding (newnum-oldnum) to
-   the existing number.  I.e. RenumberBlock(100,200) will change R213
-   to R313.
-
-   Usage: RenumberBuffer(oldnum,newnum)
-
-   Same, but the paste buffer is renumbered.
-
-*/
+/*!
+ * \file renumberblock.c
+ *
+ * \brief RenumberBlock plug-in for PCB.
+ *
+ * \author Copyright (C) 2006 DJ Delorie <dj@delorie.com>
+ *
+ * \copyright Licensed under the terms of the GNU General Public
+ * License, version 2 or later.
+ *
+ * http://www.delorie.com/pcb/renumberblock.c
+ *
+ * Compile like this:
+ *
+ * gcc -I$HOME/geda/pcb-cvs/src -I$HOME/geda/pcb-cvs -O2 -shared renumberblock.c -o renumberblock.so
+ *
+ * The resulting renumberblock.so goes in
+ * $HOME/.pcb/plugins/renumberblock.so.
+ *
+ * Usage: RenumberBlock(oldnum,newnum)
+ *
+ * All selected elements are renumbered by adding (newnum-oldnum) to
+ * the existing number.  I.e. RenumberBlock(100,200) will change R213
+ * to R313.
+ *
+ * Usage: RenumberBuffer(oldnum,newnum)
+ *
+ * Same, but the paste buffer is renumbered.
+ */
 
 #include <stdio.h>
 #include <math.h>
@@ -66,7 +70,7 @@ renumber_block (int argc, char **argv, Coord x, Coord y)
     old_ref = element->Name[1].TextString;
     for (refdes_split=cp=old_ref; *cp; cp++)
       if (!isdigit(*cp))
-	refdes_split = cp+1;
+        refdes_split = cp+1;
 
     num = atoi (refdes_split);
     num += (new_base - old_base);
@@ -76,9 +80,9 @@ renumber_block (int argc, char **argv, Coord x, Coord y)
     strcpy (new_ref + (refdes_split - old_ref), num_buf);
 
     AddObjectToChangeNameUndoList (ELEMENT_TYPE, NULL, NULL,
-				   element,
-				   NAMEONPCB_NAME (element));
-    
+                                   element,
+                                   NAMEONPCB_NAME (element));
+
     ChangeObjectName (ELEMENT_TYPE, element, NULL, NULL, new_ref);
   }
   END_LOOP;
@@ -111,7 +115,7 @@ renumber_buffer (int argc, char **argv, Coord x, Coord y)
     old_ref = element->Name[1].TextString;
     for (refdes_split=cp=old_ref; *cp; cp++)
       if (!isdigit(*cp))
-	refdes_split = cp+1;
+        refdes_split = cp+1;
 
     num = atoi (refdes_split);
     num += (new_base - old_base);
@@ -119,7 +123,7 @@ renumber_buffer (int argc, char **argv, Coord x, Coord y)
     new_ref = (char *) malloc (refdes_split - old_ref + strlen(num_buf) + 1);
     memcpy (new_ref, old_ref, refdes_split - old_ref);
     strcpy (new_ref + (refdes_split - old_ref), num_buf);
-    
+
     ChangeObjectName (ELEMENT_TYPE, element, NULL, NULL, new_ref);
   }
   END_LOOP;
